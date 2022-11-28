1fce06dcbfd18f9bb3c042e382ba8c7d964a0cfe
Michael <michaelhenriqued@gmail.com>
2022-11-28 00:27:51 -0300

    fix: show all denunciation

--- src/minhas-denuncias/minhas-denuncias.js
+++ src/minhas-denuncias/minhas-denuncias.js
@@ -10,58 +10,56 @@ function myDelete() {
 
 // Criar Denuncias
 var denuncias = JSON.parse(localStorage.getItem("denuncias"))
-console.log(denuncias)
 
-// Variável selecionando Objeto do HTML
-var divPrincipal = document.querySelector(".denounce")
+for (let i=0; i < denuncias.length; i++) {
+  // Variável selecionando Objeto do HTML
+  var divPrincipal = document.querySelector(".denounce")
 
-// Variável criando Elemento do HTML
-var card = document.createElement("div")
+  // Variável criando Elemento do HTML
+  var card = document.createElement("div")
 
-// Declarando CSS do Elemento Criado
-card.classList.add("denounce-analyses")
-
-
-// Adcionando Elemento do localStorage no HTML
-// divPrincipal.appendChild(card)
-
-// Definindo Posição dos Novos Elementos
-divPrincipal.insertBefore(card, divPrincipal.children[2])
-
-//
-var titulo = document.createElement("div")
-titulo.classList.add("title-denounce-analyses")
-
-var h2 = document.createElement("h2")
-var textoTitulo = titulo.getElementsByTagName("h2")
-
-var content = document.createTextNode(denuncias.nome)
-h2.appendChild(content)
-
-titulo.appendChild(h2)
-card.appendChild(titulo)
+  // Declarando CSS do Elemento Criado
+  card.classList.add("denounce-analyses")
 
 
-card.appendChild(document.createElement("div"))
-var resto = card.getElementsByTagName("div")[1]
-resto.setAttribute("style", "margin-left: 25px;")
+  // Adcionando Elemento do localStorage no HTML
+  // divPrincipal.appendChild(card)
 
-var ztatus = document.createElement("h3")
-var contentZtatus = document.createTextNode("Em Analise")
-ztatus.appendChild(contentZtatus)
+  // Definindo Posição dos Novos Elementos
+  divPrincipal.insertBefore(card, divPrincipal.children[2])
 
-var endereco = document.createElement("h4")
-var contentEndereco = document.createTextNode(denuncias.endereco)
-endereco.appendChild(contentEndereco)
+  //
+  var titulo = document.createElement("div")
+  titulo.classList.add("title-denounce-analyses")
 
-var historia = document.createElement("h5")
-var contentHistoria = document.createTextNode(denuncias.historia)
-historia.appendChild(contentHistoria)
+  var h2 = document.createElement("h2")
+  var textoTitulo = titulo.getElementsByTagName("h2")
 
-resto.append(ztatus, endereco, historia)
-console.log(resto)
+  var content = document.createTextNode(denuncias[i].nome)
+  h2.appendChild(content)
+
+  titulo.appendChild(h2)
+  card.appendChild(titulo)
 
 
+  card.appendChild(document.createElement("div"))
+  var resto = card.getElementsByTagName("div")[1]
+  resto.setAttribute("style", "margin-left: 25px;")
+
+  var ztatus = document.createElement("h3")
+  var contentZtatus = document.createTextNode("Em Analise")
+  ztatus.appendChild(contentZtatus)
+
+  var endereco = document.createElement("h4")
+  var contentEndereco = document.createTextNode(denuncias[i].endereco)
+  endereco.appendChild(contentEndereco)
+
+  var historia = document.createElement("h5")
+  var contentHistoria = document.createTextNode(denuncias[i].historia)
+  historia.appendChild(contentHistoria)
+
+  resto.append(ztatus, endereco, historia)
+}
 
 
 // <div class="denounce-analyses" id="d-a">
